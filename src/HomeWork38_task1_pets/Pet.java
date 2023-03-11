ad7615557ee6d9f06b7df4c0372de7e8165d5b4e
Liliia Baieva <555blv@gmail.com>
2023-03-11 22:56:02 +0100

    add constructors pet

--- src/HomeWork38_task1_pets/Pet.java
+++ src/HomeWork38_task1_pets/Pet.java
@@ -14,7 +14,33 @@ public class Pet {
 //      "кличка" (тип String),
 //      "дата рождения" (тип String),
 //  вес (тип double);
+  private Pet.Kind kindPet;
+  private String name;
+  private String dateOfBirth;
+  private double weight;
+
 //  конструктор, сеттеры, геттеры;
+  public Pet(Pet.Kind kindPet, String name){
+  this.kindPet = kindPet;
+  this.name = name;
+  this.dateOfBirth = null;
+  this.weight = 0.00;
+}
+
+  public Pet(Pet.Kind kindPet, String name, String dateOfBirth){
+    this.kindPet = kindPet;
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+    this.weight = 0.00;
+  }
+
+  public Pet(Pet.Kind kindPet, String name, String dateOfBirth, double weight){
+    this.kindPet = kindPet;
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+    this.weight = weight;
+  }
+
 //  статический метод для создания животного при прочтении данных из строки
 //  "dog,кличка", "cat,кличка,вес", "turtle,кличка,вес,дата рождения".
 //  Создать класс Main, в котором данные будут считываться с консоли и красиво выводиться на экран.
