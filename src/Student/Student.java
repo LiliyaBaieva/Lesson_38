5c8a8c70606f4cfe963e5dfbda08fff0a1105a6c
Liliia Baieva <555blv@gmail.com>
2023-03-11 19:44:50 +0100

    add getters and setters in Student

---
+++ src/Student/Student.java
@@ -0,0 +1,48 @@
+package Student;
+
+public class Student {
+
+  private String name;
+  private String group;
+  private String eMail;
+
+//  String eMail = null;
+
+  public Student(String name, String group){
+    this.name = name;
+    this.group = group;
+    this.eMail = null;
+  }
+
+  public Student(String name, String group, String eMail){
+    this.name = name;
+    this.group = group;
+    this.eMail = eMail;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getGroup() {
+    return group;
+  }
+
+  public String getEMail() {
+    return eMail;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setGroup(String group) {
+    this.group = group;
+  }
+
+  public void setEMail(String eMail) {
+    this.eMail = eMail;
+  }
+
+
+}
