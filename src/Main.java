2f94689c53b578d3d3d0d08288a1a826c302afb1
Liliia Baieva <555blv@gmail.com>
2023-03-11 20:35:30 +0100

    add new student and print student

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import Student.Student;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // написать базу данных студентов
+    // студенты могут находиться в группах
+
+    // красиво вывести состав групп на экран
+    // - прочитать название группы
+    // - прочитать количество студентов
+    // - прочитать информацию о студентах - "имя" или "имя,e-mail" для каждого в отдельной строке
+
+    Student student = new Student("Кирилл", "");
+    Student student1 = Student.parseStudent("cohort24", "Liliia,liliia@gmail.com");
+    System.out.println(student1.getName() + ", " + student1.getGroup() + ", " + student1.getEMail());
+
   }
 }
