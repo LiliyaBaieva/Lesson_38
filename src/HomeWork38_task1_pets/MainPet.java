42537a58cae5aabffc01a31edf36b57ad2548ad5
Liliia Baieva <555blv@gmail.com>
2023-03-13 16:54:44 +0100

    add list of pets

---
+++ src/HomeWork38_task1_pets/MainPet.java
@@ -0,0 +1,27 @@
+package HomeWork38_task1_pets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainPet {
+//  Создать класс Main, в котором данные будут считываться с консоли и красиво выводиться на экран.
+//  Формат входных данных: , затем сами записи в описанном формате.
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    List<Pet> pets = new ArrayList<>();
+//    количество строк с записями о домашних животных
+    int numberPets = Integer.parseInt(br.readLine());
+    String line;
+    for (int i = 0; i < numberPets; ++i){
+      line = br.readLine();
+      Pet pet = Pet.parsePet(line);
+      pets.add(pet);
+
+
+  }
+
+
+}
